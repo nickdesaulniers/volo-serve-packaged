6c4d50ba211666e9b52ad16d8dd4239944587f1d
Nick Desaulniers <ndesaulniers@mozilla.com>
2013-01-16 16:44:10 -0800

    spacing

--- index.js
+++ index.js
@@ -2,32 +2,19 @@ var fs = require('fs'),
     path = require('path');
 
 module.exports = {
-    summary: 'A sample volo command that prints a greeting.',
+  summary: 'Serve packaged Firefox OS apps for rapid development using volo.',
 
-    //This is a longer form version of the help. markdown
-    //is the suggested file format to use.
-    doc: fs.readFileSync(path.join(__dirname, 'doc.md'), 'utf8'),
+  //This is a longer form version of the help. markdown
+  //is the suggested file format to use.
+  doc: fs.readFileSync(path.join(__dirname, 'doc.md'), 'utf8'),
 
-    flags: {
-        'u': 'upper'
-    },
+  flags: {
+    'p': 'port'
+  },
 
-    //Optional, validate the arguments
-    validate: function (namedArgs, arg1, arg2) {
-        if (!arg1) {
-            return new Error('Please enter a greeting');
-        }
-        if (!arg2) {
-            return new Error('Please enter a name.');
-        }
-    },
-
-    run: function (d, v, namedArgs, arg1, arg2) {
-        var message = arg1 + ' ' + arg2 + '!';
-        if (namedArgs.upper) {
-            message = message.toUpperCase();
-        }
-
-        d.resolve(message);
-    }
+  run: function (d, v, namedArgs, arg1) {
+    var port = namedArgs.port ? arg1 : 3000;
+    console.log('Will run on port ' + port);
+    d.resolve('done');
+  }
 };
